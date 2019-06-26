7c6a9694f2f7b0b2d3144eb050221d3173e39f16
변재욱 <>
2019-06-26 14:52:59 +0900

    initial commit

---
+++ src/main/java/com/example/demo/PureHelloWorld1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureHelloWorld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureHelloWorld1Application.class, args);
+	}
+
+}
